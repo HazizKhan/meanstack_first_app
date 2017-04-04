cc763c43230ecb1b7474a1aa4397e34591a834b2
shadabahmedkhan <shadab33@hotmail.com>
2017-04-05 01:37:07 +0500

    meanstack

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import  {RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import {PostsService} from './posts.service';
+const Routes =[
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(Routes) // add routes to the app
+  ],
+  providers: [PostsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
